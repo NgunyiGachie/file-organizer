2783eb407fb04a5eb53d37f49bb4f8c82a46bf07
NgunyiGachie <antogachie@gmail.com>
2025-02-25 21:05:18 +0300

    video code

--- organizer.sh
+++ organizer.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 #images
-find . -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png \) -exec sh -c '
+find . -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.gif \) -exec sh -c '
 for file do
     dir=$(dirname "$file")
     mkdir -p "$dir/images"
     mv "$file" "$dir/images/"
+done
+' sh {} +
+
+#videos
+find . -type f \( -iname \*.mkv -0 -iname \*.mp4 -o -iname \*.avi \) -exec sh -c '
+for file do
+    dir=$(dirname "$file")
+    mkdir -p "$dir/Videos"
+    mv "$file" "$dir/Videos/"
 done
 ' sh {} +
